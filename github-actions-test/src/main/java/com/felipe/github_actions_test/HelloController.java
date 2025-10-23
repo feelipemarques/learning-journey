e3e92b6bcc4734fba254059a534685c8d6108fb5
feelipemarques <feelipemarquees@gmail.com>
2025-10-23 14:49:24 -0300

    Creating the tests

--- github-actions-test/src/main/java/com/felipe/github_actions_test/HelloController.java
+++ github-actions-test/src/main/java/com/felipe/github_actions_test/HelloController.java
@@ -1,4 +1,13 @@
 package com.felipe.github_actions_test;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello, world!";
+    }
 }
--- github-actions-test/src/test/java/com/felipe/github_actions_test/HelloControllerTest.java
+++ github-actions-test/src/test/java/com/felipe/github_actions_test/HelloControllerTest.java
@@ -1,4 +1,15 @@
 package com.felipe.github_actions_test;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class HelloControllerTest {
+
+    @Test
+    void testHello(){
+        HelloController helloController = new HelloController();
+        String response = helloController.hello();
+        assertThat(response).isEqualTo("Hello, world!");
+    }
 }
